325c23117a34c99b5d638dd67e420be8f73d290e
Dante Corengia <dante.corengia@gmail.com>
2024-03-26 04:17:13 -0300

    detalles

--- js/script.js
+++ js/script.js
@@ -19,3 +19,13 @@ punto.forEach( ( cadaPunto , i )=> {
 
     })
 })
+
+// Registro ventana
+
+document.getElementById('openPopup').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'block';
+});
+
+function cerrarPopup() {
+    document.getElementById('popup').style.display = 'none';
+}

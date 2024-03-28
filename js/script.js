a8c2b10b57e301209ff65b1abf10f0603eaad231
Dante Corengia <dante.corengia@gmail.com>
2024-03-28 04:09:18 -0300

    ultimos detalles esteticos

--- js/script.js
+++ js/script.js
@@ -30,15 +30,40 @@ function cerrarPopup() {
     document.getElementById('popup').style.display = 'none';
 }
 
-
+function registroExitoso() {
+    var nombre = document.getElementById('nombre').value;
+    var email = document.getElementById('email').value;
+    var contrasena = document.getElementById('contrasena').value;
+  
+    if (nombre !== '' && email !== '' && contrasena !== '') {
+      alert("¡Registrado con éxito!");
+      cerrarPopup();
+      return false;
+    } 
+}
 // Boton
 
 function openPopup() {
     var popup = document.getElementById("popup");
-    popup.style.display = "block"; // Mostrar la ventana emergente
+    popup.style.display = "block"; // Mostrar la ventana 
 }
 
 function cerrarPopup() {
     var popup = document.getElementById("popup");
-    popup.style.display = "none"; // Ocultar la ventana emergente
+    popup.style.display = "none"; // Ocultar la ventana 
 }
+
+// Mensaje enviado
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+
+    if (name.trim() !== '' && email.trim() !== '' && message.trim() !== '') {
+        // Mostrar la ventana 
+        alert("¡Mensaje enviado con éxito!")
+        // Evitar que el formulario se envíe
+        event.preventDefault();
+    }
+});
